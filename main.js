7f1951b02ca2c908a626f5883124f1bb2a013d82
Melinn Phifer <mphifer@ufl.edu>
2020-10-14 13:40:17 -0400

    append data to one line

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var weatherMessage = document.getElementById('weather');
-
 var headerCity = document.getElementById('city');
+
+var weatherMessage = document.getElementById('weather');
 
 
 var xmlhttp = new XMLHttpRequest();
@@ -8,21 +8,17 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var apiResult = JSON.parse(this.responseText);
 
-  //print city name in header text
+      //print city name in header text
         function showCity() {
-          var cityName = document.createElement('h1');
-          headerCity.appendChild(cityName);
           var cityText = document.createTextNode(apiResult.name);
-          cityName.appendChild(cityText);
+          headerCity.appendChild(cityText);
         };
         showCity();
 
-  //print weather at the end of main section text
+      //print weather at the end of main section text
         function showWeather() {
-          var displayWeather = document.createElement('p');
-          weatherMessage.appendChild(displayWeather);
-          var weatherToday = document.createTextNode(apiResult.weather[0].description);
-          displayWeather.appendChild(weatherToday);
+          var weatherText = document.createTextNode(apiResult.weather[0].description);
+          weatherMessage.appendChild(weatherText);
         };
         showWeather();
     }
